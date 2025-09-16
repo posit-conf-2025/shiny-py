64a48ebb287e0fd7908c542f51b4b4a946d40eb4
Daniel Chen <chendaniely@users.noreply.github.com>
2025-09-16 00:39:42 -0400

    add in comments for openai code w/github

--- code/03-llm/01-basics/github-openai.py
+++ code/03-llm/01-basics/github-openai.py
@@ -6,25 +6,39 @@ from openai import OpenAI
 
 load_dotenv()
 
-# arguments passed switch to using GitHub models
+# Creates an OpenAI client, which can be used to access any OpenAI service
+# (including Whisper and DALL-E, not just chat models). It's totally stateless.
 client = OpenAI(
-  api_key=os.environ["GITHUB_TOKEN"],
-  base_url="https://models.inference.ai.azure.com"
+    api_key=os.environ["GITHUB_TOKEN"],
+    base_url="https://models.github.ai/inference",
 )
 
+# The initial set of messages we'll start the conversation with: a system
+# prompt and a user prompt.
 messages = [
     {"role": "system", "content": "You are a terse assistant."},
     {"role": "user", "content": "What is the capital of the moon?"},
 ]
 
+# Call out to the OpenAI API to generate a response. (This is a blocking call,
+# but there are ways to do async, streaming, and async streaming as well.)
 response = client.chat.completions.create(
-    model="gpt-4.1",
+    model="openai/gpt-5",
     messages=messages,
 )
-print(response.choices[0].message.content)
 
+# Print the response we just received.
+print(response.choices[0].message.content)
+# If you want to inspect the full response, you can do so by uncommenting the
+# following line. The .dict() is helpful in getting more readable output.
+# pprint(response.dict())
+
+# The client.chat.completions.create() call is stateless. In order to carry on a
+# multi-turn conversation, we need to keep track of the messages we've sent and
+# received.
 messages.append(response.choices[0].message)
 
+# Ask a followup question.
 messages.append({"role": "user", "content": "Are you sure?"})
 response2 = client.chat.completions.create(
     model="gpt-4.1",
