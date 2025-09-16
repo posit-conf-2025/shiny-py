a532034825240050430c74b5aed05985a2899e94
Daniel Chen <chendaniely@users.noreply.github.com>
2025-09-16 00:41:11 -0400

    remove workshop code folder (used in code)

--- workshop/chatlas.py
+++
@@ -1,8 +0,0 @@
-from chatlas import ChatOpenAI
-from dotenv import load_dotenv
-
-chat = ChatOpenAI(model="gpt-4.1", system_prompt="You are a terse assistant.")
-
-chat.chat("What is the capital of the moon?")
-
-chat.chat("Are you sure?")
